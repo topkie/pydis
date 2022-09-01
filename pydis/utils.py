0fd9a194fccf0c6e56619884d8c9992a46ee0779
topkie <sohusi@outlook.com>
2022-09-02 00:56:46 +0800

    feat: 单例，但多变
    通过定制元类，使得通过初始化传参改变 Pydis 全局失效时间成为可能

--- pydis/utils.py
+++ pydis/utils.py
@@ -4,10 +4,19 @@ class Singleton(type):
     def __init__(self, *args, **kwargs):
         self.__instance = None
         super().__init__(*args, **kwargs)
-        
+
     def __call__(self, *args, **kwargs):
         if self.__instance is None:
             self.__instance = super().__call__(*args, **kwargs)
             return self.__instance
         else:
+            if not args:
+                return self.__instance
+            if len(args) != 1 or kwargs:
+                raise TypeError(
+                    '__init__() takes from 1 to 2 ' +
+                    'positional arguments but %d were given'
+                    % (len(args) + len(kwargs))
+                )
+            self.__instance.default_timeout = args[0]
             return self.__instance
