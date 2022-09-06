b7f7aef0d3e175d830d2977d74a6dd094ed33d9b
topkie <sohusi@outlook.com>
2022-09-06 21:18:38 +0800

    fix: 修复了连接被关闭后 recv 函数仍被阻塞的错误

--- pydis/multithreading/connection.py
+++ pydis/multithreading/connection.py
@@ -2,10 +2,13 @@
 
 from queue import Empty, Queue
 from threading import Event
-from typing import Any, Optional, Tuple
+from typing import Optional, Tuple
 
 from ..exceptions import ConnectionClosedError, ReceiveTimeout
 from .typing import MessageT
+
+
+CLOSE = '#CLS'
 
 
 class Connection:
@@ -25,7 +28,7 @@ class Connection:
 
     def send(self, data: MessageT):
         '''发送数据
-        
+
         当连接被关闭时，引发 ConnectionClosed 错误。
         这个错误在 pydis.exceptions 中定义
         '''
@@ -39,15 +42,17 @@ class Connection:
         '''从连接中接收数据
 
         如果设定了超时，超时时会抛出 ReceiveTimeout
-        
-        当连接被关闭时，抛出 ConnectionClosed
-        
+
+        当连接被关闭时，抛出 ConnectionClosedError
+
         这些错误在 pydis.exceptions 中定义
         '''
         if self.closed:
             raise ConnectionClosedError('connection has been closed')
         try:
             ret = self.q_recv.get(block, timeout)
+            if ret is CLOSE:
+                raise ConnectionClosedError('connection has been closed')
             self.q_recv.task_done()
             return ret
         except Empty:
@@ -55,6 +60,7 @@ class Connection:
 
     def close(self):
         self._closed.set()
+        self.q_send.put(CLOSE)
 
     @property
     def closed(self):
--- tests/test_multithreading/test_connection.py
+++ tests/test_multithreading/test_connection.py
@@ -20,6 +20,23 @@ class TestConnection(TestCase):
         with self.assertRaises(ReceiveTimeout):
             conn1.recv(timeout=.1)
 
+    def test_recv_ConnectionClosedError_while_closed(self):
+        # 测试 recv 函数在等待数据时连接被关闭
+        # 应该抛出 ConnectionClosedError
+        from threading import Thread, Event
+        from time import sleep
+
+        def close_thread(conn, signal):
+            signal.wait()
+            sleep(.1)  # 等待另一端准备好接收数据
+            conn.close()
+        conn1, conn2 = open_connection()
+        close_signal = Event()
+        Thread(target=close_thread, args=(conn2, close_signal), daemon=True).start()
+        with self.assertRaises(ConnectionClosedError):
+            close_signal.set()
+            conn1.recv()
+
     def test_close(self):
         conn1, conn2 = open_connection()
         conn1.close()
